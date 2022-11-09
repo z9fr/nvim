512bd900a811daaf99ced80272fe05853acdc944
z9fr <z9fr@protonmail.com>
2022-11-09 18:35:24 +0530

    docs: some docs and updates

--- lua/dasith/packer.lua
+++ lua/dasith/packer.lua
@@ -3,6 +3,28 @@ return require("packer").startup(function(use)
     use 'folke/tokyonight.nvim'
 
     use({ 'rose-pine/neovim', as = 'rose-pine' })
-     use({"catppuccin/nvim", as = "catppuccin" })
+    use({"catppuccin/nvim", as = "catppuccin" })
+
+    -- All the things
+    use("neovim/nvim-lspconfig")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
+    use "hrsh7th/nvim-cmp" --completion
+ 	use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+    use("onsails/lspkind-nvim")
+    use("nvim-lua/lsp_extensions.nvim")
+    use("glepnir/lspsaga.nvim")
+    use("simrat39/symbols-outline.nvim")
+    use("L3MON4D3/LuaSnip")
+    use("saadparwaiz1/cmp_luasnip")
+
+    
+    -- treesitter
+    use("nvim-treesitter/nvim-treesitter", {
+        run = ":TSUpdate"
+    })
+
+    use("nvim-treesitter/playground")
+    use("romgrk/nvim-treesitter-context")
 end)
 
---
+++ notes.md
@@ -0,0 +1,13 @@
+## Code functions
+
+- K -> view description (hover the function)
+- gd -> go to definition
+
+
+## Auto completion 
+
+- ctrl + y -> will autocomplete
+- ctrl + u -> will scroll down on docs
+- ctrl + d -> will scroll down on docs 
+
+
