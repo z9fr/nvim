44fee9d674f9c5a5082ab8519ca48b3c93809cbd
z9fr <z9fr@protonmail.com>
2023-06-03 14:40:19 +0530

    feat: setup lsp

--- lua/dasith/packer.lua
+++ lua/dasith/packer.lua
@@ -11,7 +11,6 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-
   use({ 
 	  'rose-pine/neovim',
 	  as = 'rose-pine' 
@@ -21,6 +20,29 @@ return require('packer').startup(function(use)
 
    use('nvim-treesitter/nvim-treesitter')
    use('nvim-treesitter/playground')
+   use("theprimeagen/harpoon")
+   use("mbbill/undotree")
+
+   use {
+	   'VonHeikemen/lsp-zero.nvim',
+	   branch = 'v2.x',
+	   requires = {
+		   -- LSP Support
+		   {'neovim/nvim-lspconfig'},             -- Required
+		   {                                      -- Optional
+		   'williamboman/mason.nvim',
+		   run = function()
+			   pcall(vim.cmd, 'MasonUpdate')
+		   end,
+	   },
+	   {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	   -- Autocompletion
+	   {'hrsh7th/nvim-cmp'},     -- Required
+	   {'hrsh7th/cmp-nvim-lsp'}, -- Required
+	   {'L3MON4D3/LuaSnip'},     -- Required
+   	}
+   }
 
    use {
 	   'nvim-treesitter/nvim-treesitter',
