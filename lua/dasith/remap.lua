3d2b1e230ff179c3e4c502e217a2c3a80567948e
z9fr <z9fr@protonmail.com>
2023-06-04 23:09:32 +0530

    feat: added some remaps

--- lua/dasith/remap.lua
+++ lua/dasith/remap.lua
@@ -1,2 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-i>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
